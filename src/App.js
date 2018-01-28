7e53903b3bba4f3d6d8e894dfece7d43e1e4b3eb
Adam Miclot <amiclot09@gmail.com>
2018-01-28 12:50:52 -0500

    added projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Nav from "./components/Nav";
 import { Container, Row, Col } from "./components/Grid";
 import Card from "./components/Card";
 import Logo from "./components/Logo";
+import port_img from "./images/portfolio_img.jpg";
+import bjj_img from "./images/transcendbjj.JPG";
+import healthdash_img from "./images/healthdash.JPG";
+import geek_img from "./images/geek.JPG";
+import OR_img from "./images/open-rolls.JPG";
 import "./App.css";
 
 class App extends Component {
@@ -41,18 +46,42 @@ class App extends Component {
                     <Row>
                       <Col size="xs-6">
                         <Card>
-
+                          <h2>Solo Project</h2>
+                          <div class="mainmain">
+                            <a href="https://github.com/amiclot/open-rolls"><img class="egger" src={OR_img} alt="port_img" />
+                            <div class="texter">
+                              A working project, for Brazilian Jiu Jitsu athletes to find open training sessions in their city or when they travel. The front-end is built with ReactJS and the back end is done in MongoDB using Mongoose. I did the planning and execution for this app, learning the MERN stack in the process. CLICK to see this project's GitHub repository.
+                            </div></a>
+                          </div>
                         </Card>
                         <Card>
-
+                          <h2>Collaborative Project</h2>
+                          <div class="mainmain">
+                            <a href="https://github.com/Button-Mashers/Guide-A-Geek"><img class="egger" src={geek_img} alt="port_img" />
+                            <div class="texter">
+                              A dashboard for the Assassin’s Creed Origins video game. The app was built with a MySQL  back-end and utilized Embedded Javascript for templating. The app also included a web scraper using cheerio. My responsibilities were everything except for the web scraper, and some of the front-end design. Including user authentication, route creation, and front-end templating. CLICK to see this project's GitHub repository.
+                            </div></a>
+                          </div>
                         </Card>
                       </Col>
                       <Col size="xs-6">
                         <Card>
-
+                          <h2>Collaborative Project</h2>
+                          <div class="mainmain">
+                            <a href="https://github.com/the-code-whisperers/project"><img class="egger" src={healthdash_img} alt="port_img" />
+                            <div class="texter">
+                              A calorie  tracker made for user to track their daily caloric intake and caloric exercise output.  The app includes user authentication (Firebase), Youtube API to pull relevant exercise videos depending on user selection, Nutritionix API, and Bideo.js plugin to add styling to the user registration page. My responsibilities were building the Youtube API and overall front-end design/code. CLICK to see this project's GitHub repository.
+                            </div></a>
+                          </div>
                         </Card>
                         <Card>
-
+                          <h2>Solo Project</h2>
+                          <div class="mainmain">
+                            <a href="https://amiclot.github.io/"><img class="egger" src={bjj_img} alt="port_img" />
+                            <div class="texter">
+                              I created this site early in the Case Western program to practice JQuery, HTML5 and CSS3. The site was a request from a colleague planning to open a Brazilian Jiu Jitsu bussiness. I would later use some of the styling from this project as compoenents in my final fullstack ReactJS app "open-rolls". CLICK to go to Site.
+                            </div></a>
+                          </div>
                         </Card>
                       </Col>
                     </Row>
